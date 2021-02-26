e480d2df1434a6099b9ee1d5724f2f42658f1f12
icekunx <icekunxp@gmail.com>
2021-02-26 13:31:18 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -69,7 +69,7 @@ if(!is_null($events)){
                     break;
                 case "ภาพ":
                     $picFullSize = 'https://sv1.picz.in.th/images/2021/02/15/oQ57aW.md.jpg';
-                    $picThumbnail = 'www.google.com';
+                    $picThumbnail = 'https://sv1.picz.in.th/images/2021/02/15/oQ57aW.md.jpg';
                     $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
                     break;
                 case "วีดีโอ":
